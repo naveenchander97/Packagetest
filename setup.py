43d917d5109b12cc26d59d9441c181352eb8e35e
naveenchander97 <45679552+naveenchander97@users.noreply.github.com>
2023-07-28 21:30:28 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='fetchdatetime',
-    version='0.1',
-    description='Retrieves current date and time',
-    author='Naveen',
-    packages=find_packages(where="currentdatetime")
-)
