e4ee78162e1611eb1cbb1912d4a74a667675e1f0
naveenchander97 <45679552+naveenchander97@users.noreply.github.com>
2023-07-28 20:20:45 +0530

    Added first package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fetchdatetime',
+    version='0.1',
+    description='Retrieves current date and time',
+    author='Naveen',
+    packages=find_packages(where="currentdatetime")
+)
